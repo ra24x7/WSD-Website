17384216fd9552806a1622b16409a1eca40fba98
Rajat Agrawal <r.agrawal247@gmail.com>
2023-04-17 01:21:34 +0545

    Update wsdscript.js

--- wsdscript.js
+++ wsdscript.js
@@ -1,17 +1,1 @@
-const gifLogo = document.getElementById('.wsdlogo img ');
-const loopCount = 1; // change this value to set the number of loops before stopping
 
-gifLogo.addEventListener('load', function() {
-  let loopCounter = 0;
-
-  const loop = setInterval(function() {
-    loopCounter++;
-
-    if (loopCounter >= loopCount) {
-      clearInterval(loop);
-      setTimeout(function() {
-        gifLogo.src = gifLogo.src;
-      }, 100); // change this delay value if necessary
-    }
-  }, gifLogo.duration);
-});
